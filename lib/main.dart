38c5801a0ceacf4b8ab694588681d5f88436d0b7
Ibrahim Hatchabi <i.hatchabi@finetechgroupe.com>
2021-11-12 15:47:13 +0000

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,28 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'package:flutter/material.dart';
+import 'package:todoapp/screens/home_screen.dart';
+import 'package:todoapp/screens/main_layout.dart';
+import 'package:todoapp/screens/menu_screen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'TodoApp',
+      theme: ThemeData(
+        primarySwatch: Colors.green,
+      ),
+      // home: MenuScreen(),
+      home: MainLayout(),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
